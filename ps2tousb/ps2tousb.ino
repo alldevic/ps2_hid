5066e448bf5c13541676e3e5e90c71d9d53cff91
Nikolay Bely <belyynikolay@gmail.com>
2017-07-23 19:37:01 +0700

    add draft bt support

--- ps2tousb/ps2tousb.ino
+++ ps2tousb/ps2tousb.ino
@@ -9,8 +9,20 @@ static ps2::Keyboard<2,3,16, ps2::NullDiagnostics> ps2Keyboard(diagnostics);
 static ps2::UsbKeyboardLeds ledValueLastSentToPs2 = ps2::UsbKeyboardLeds::none;
 
 void setup() {
+    Serial1.begin(115200);
     ps2Keyboard.begin();
     BootKeyboard.begin();
+}
+
+void BTkeyboardPress(byte BP_KEY,byte BP_MOD){
+  Serial1.write((byte)0xFD); //Start HID Report
+  Serial1.write((byte)0x9); //Length byte
+  Serial1.write((byte)0x1); //Descriptor byte
+  Serial1.write(BP_MOD); //Modifier byte
+  Serial1.write((byte)0x00); //-
+  Serial1.write(BP_KEY); //Send KEY
+  for(byte i = 0;i<5;i++) //Send five zero bytes
+    Serial1.write((byte)0x00);
 }
 
 void loop() {
@@ -29,9 +41,11 @@ void loop() {
         switch (action.gesture) {
             case ps2::UsbKeyAction::KeyDown:
                 BootKeyboard.press(hidCode);
+                BTkeyboardPress(hidCode,(byte)0x00);     
                 break;
             case ps2::UsbKeyAction::KeyUp:
                 BootKeyboard.release(hidCode);
+                BTkeyboardPress((byte)0x00,(byte)0x00);
                 break;
         }
     }
