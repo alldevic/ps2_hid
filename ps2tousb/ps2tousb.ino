431b556b6fc407cde9355c919d00076ec888d504
Nikolay Bely <belyynikolay@gmail.com>
2017-08-04 22:29:04 +0700

    Remove bt, add consumer keys

--- ps2tousb/ps2tousb.ino
+++ ps2tousb/ps2tousb.ino
@@ -9,22 +9,12 @@ static ps2::Keyboard<2,3,16, ps2::NullDiagnostics> ps2Keyboard(diagnostics);
 static ps2::UsbKeyboardLeds ledValueLastSentToPs2 = ps2::UsbKeyboardLeds::none;
 
 void setup() {
-    Serial1.begin(115200);
     ps2Keyboard.begin();
+    Consumer.begin();
     BootKeyboard.begin();
 }
 
-void BTkeyboardPress(byte BP_KEY,byte BP_MOD){
-  Serial1.write((byte)0xFD); //Start HID Report
-  Serial1.write((byte)0x9); //Length byte
-  Serial1.write((byte)0x1); //Descriptor byte
-  Serial1.write(BP_MOD); //Modifier byte
-  Serial1.write((byte)0x00); //-
-  Serial1.write(BP_KEY); //Send KEY
-  for(byte i = 0;i<5;i++) //Send five zero bytes
-    Serial1.write((byte)0x00);
-}
-
+static bool fn = false;
 void loop() {
     ps2::UsbKeyboardLeds newLedState = (ps2::UsbKeyboardLeds)BootKeyboard.getLeds();
     if (newLedState != ledValueLastSentToPs2) {
@@ -37,16 +27,27 @@ void loop() {
     else if (scanCode != ps2::KeyboardOutput::none) {
         ps2::UsbKeyAction action = keyMapping.translatePs2Keycode(scanCode);
         KeyboardKeycode hidCode = (KeyboardKeycode)action.hidCode;
-
+               
         switch (action.gesture) {
             case ps2::UsbKeyAction::KeyDown:
-                BootKeyboard.press(hidCode);
-                BTkeyboardPress(hidCode,(byte)0x00);     
-                break;
+              if (hidCode == KEY_RIGHT_ALT) fn = true;
+              if (fn){
+                if (hidCode == KEY_DOWN) Consumer.write(MEDIA_PLAY_PAUSE);  
+                if (hidCode == KEY_RIGHT) Consumer.write(MEDIA_NEXT);  
+                if (hidCode == KEY_LEFT) Consumer.write(MEDIA_PREV);  
+                if (hidCode == KEY_UP) Consumer.write(MEDIA_STOP), fn = false;  
+                if (hidCode == KEY_F10) Consumer.write(MEDIA_VOL_MUTE), fn = false;  
+                if (hidCode == KEY_F11) Consumer.write(MEDIA_VOL_DOWN);  
+                if (hidCode == KEY_F12) Consumer.write(MEDIA_VOL_UP);
+                if (hidCode == KEY_F8) Consumer.write(CONSUMER_CALCULATOR), fn = false;
+              } else BootKeyboard.press(hidCode);
+              break;
             case ps2::UsbKeyAction::KeyUp:
-                BootKeyboard.release(hidCode);
-                BTkeyboardPress((byte)0x00,(byte)0x00);
-                break;
+              if (hidCode == KEY_RIGHT_ALT) fn = false;
+                else BootKeyboard.release(hidCode);
+                
+              break;
+            
         }
     }
 }
